9ac08d9f4be0bfecb96a32539dbb705ca071ce5d
meportillo <mauroeportillo@gmail.com>
2017-05-18 20:33:18 -0300

    issue #24 documentacion: corregir relaciones

--- GestorOdontologico/src/main/webapp/app/js/agregarPacienteCtrl.js
+++ GestorOdontologico/src/main/webapp/app/js/agregarPacienteCtrl.js
@@ -4,8 +4,8 @@ app.controller('agregarPacienteCtrl',function($scope, $http, toaster, PacienteSe
  	$scope.paciente.anios = 0;
 
 	$scope.myDate = new Date();
-	$scope.minDate = new Date($scope.myDate.getFullYear(), $scope.myDate.getMonth() - 2, $scope.myDate.getDate());
-	$scope.maxDate = new Date($scope.myDate.getFullYear(),$scope.myDate.getMonth() + 2, $scope.myDate.getDate());
+//	$scope.minDate = new Date($scope.myDate.getFullYear(), $scope.myDate.getMonth() - 2, $scope.myDate.getDate());
+//	$scope.maxDate = new Date($scope.myDate.getFullYear(),$scope.myDate.getMonth() + 2, $scope.myDate.getDate());
 	$scope.hoy = new Date();
 
 	
