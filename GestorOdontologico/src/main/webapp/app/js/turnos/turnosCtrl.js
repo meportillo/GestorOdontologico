79da156e97c479f10e403706ecb4c2eb557ec5c6
meportillo <mauroeportillo@gmail.com>
2017-06-05 22:46:41 -0300

    parte del cambio de idioma i18n estatico de 'en' a 'es'

--- GestorOdontologico/src/main/webapp/app/js/turnos/turnosCtrl.js
+++ GestorOdontologico/src/main/webapp/app/js/turnos/turnosCtrl.js
@@ -1,7 +1,8 @@
-angular.module('mwl.calendar.docs', ['mwl.calendar', 'ngAnimate', 'ui.bootstrap', 'colorpicker.module']);
+angular.module('mwl.calendar.docs', ['mwl.calendar', 'ngAnimate', 'ui.bootstrap', 'colorpicker.module','oc.lazyLoad']);
 angular
   .module('mwl.calendar.docs') //you will need to declare your module with the dependencies ['mwl.calendar', 'ui.bootstrap', 'ngAnimate']
-  .controller('KitchenSinkCtrl', function(moment, calendarConfig, $http, toaster, $scope, TurnoService) {
+  .controller('KitchenSinkCtrl',['moment', 'calendarConfig', '$http', 'toaster', '$scope', 'TurnoService', '$window', '$ocLazyLoad',
+	  function(moment, calendarConfig, $http, toaster, $scope, TurnoService,  $window, $ocLazyLoad) {
 
     var vm = this;
 
@@ -132,6 +133,27 @@ else{*/
     };
     
     
+    calendarConfig.dateFormatter = 'moment'; // use moment instead of angular for formatting dates
+    var originali18n = angular.copy(calendarConfig.i18nStrings);
+    calendarConfig.i18nStrings.weekNumber = 'Semaine {week}';
+    
+    $window.moment = $window.moment || moment;
+    $ocLazyLoad.load('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/locale/es.js').then(function() {
+      moment.locale('es', {
+        week: {
+          dow: 1 // Monday is the first day of the week
+        }
+      });
+      moment.locale('es'); // change the locale to french
+    });
+
+    $scope.$on('$destroy', function() {
+      moment.locale('en');
+      calendarConfig.i18nStrings = originali18n;
+    });
+
+    
+    
 //    TABLE DE DATOS - TUrNOS
 	TurnoService.obtenerTodosLosTurnos()
 	.then(function (turnos) {
@@ -139,5 +161,7 @@ else{*/
 	});
 
 	
+	
+	
     
-  });
+  }]);
